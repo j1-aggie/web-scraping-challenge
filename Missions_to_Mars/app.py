fa445ce7c066cd8f17dd0ea30fba7aac14410328
Jeremy jones <j1-aggie@hotmail.com>
2020-10-29 19:07:14 -0500

    final

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -35,42 +34,4 @@ def scrape():
 
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_mars
-
-# Create an instance of Flask
-app = Flask(__name__)
-
-
-# Use flask_pymongo to set up mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
-# Route to render index.html template using data from Mongo
-@app.route("/")
-def index():
-
-    # Find one record of data from the mongo database
-    mars_info = mongo.db.mars_collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", mars_data=mars_info)
-
-#scrape route
-@app.route("/scrape")
-def scrape():
-  
-   
-    mars_data = scrape_mars.scrape()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.mars_collection.update({}, mars_data, upsert=True)
-    
-    #go back to the home page
-    return redirect("/")
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> 7abf0b4756df32746fe122e24305994ea5878bab
     #app.run(host='0.0.0.0',port=5000)
